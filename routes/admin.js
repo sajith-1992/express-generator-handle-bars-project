f8ca3f8f0346875d08d2b271e72812149b646845
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2024-07-09 13:17:42 -0400

    add all products

--- routes/admin.js
+++ routes/admin.js
@@ -1,33 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var productHelper = require('../helpers/producthelpers')
+var productHelper = require('../helpers/productHelper')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  let product=[{name:"iphone 15 prox",
-  category:"phone case",
-  price:"10 CAD",
-  image:"https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRfztewwfpbqL71Pt0l3w8CaeqpOOQakp6H1gU-deaIMPogS2xrs3vyDfBx_Vc9EHK9EB0ZHtTugvl6QWHjkiyPIy5egMlTEEXgmkFWUWGcSVMkYAYCAgkd6A&usqp=CAE"
-
-},{name:"i phone 13 pro",
-category:"phone case",
-price:"20 CAD"},{
-name:"i phone 12",
-category:"phone case",
-price:"40 cad",
-image:"https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRfztewwfpbqL71Pt0l3w8CaeqpOOQakp6H1gU-deaIMPogS2xrs3vyDfBx_Vc9EHK9EB0ZHtTugvl6QWHjkiyPIy5egMlTEEXgmkFWUWGcSVMkYAYCAgkd6A&usqp=CAE"
-},{
-name:"i phone 11 pro",
-category:"phone case",
-price:"30 cad",
-image:"https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRfztewwfpbqL71Pt0l3w8CaeqpOOQakp6H1gU-deaIMPogS2xrs3vyDfBx_Vc9EHK9EB0ZHtTugvl6QWHjkiyPIy5egMlTEEXgmkFWUWGcSVMkYAYCAgkd6A&usqp=CAE"
-}, {
-name:"i phone 10 pro",
-category:"phone case",
-price:"40 cad",
-image:"https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRfztewwfpbqL71Pt0l3w8CaeqpOOQakp6H1gU-deaIMPogS2xrs3vyDfBx_Vc9EHK9EB0ZHtTugvl6QWHjkiyPIy5egMlTEEXgmkFWUWGcSVMkYAYCAgkd6A&usqp=CAE"
-}]
-  res.render('admin/view-products',{product,admin:true});
+  productHelper.getAllProducts().then((allProducts)=>{
+    console.log(allProducts)
+    res.render('admin/view-products',{allProducts,admin:true})
+  
+  })
+  
 });
 router.get('/add-product',(req,res)=>{
   res.render("admin/add-product",{admin:true})
